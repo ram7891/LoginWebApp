8790defa89090f0fe3497d4c843bf340810d46bd
krishna <ramakrishna7891@gmail.com>
2016-10-24 00:45:45 +0530

    commiting one.java in to repo

---
+++ src/main/java/citi/one.java
@@ -0,0 +1,13 @@
+package citi;
+
+/**
+ * Hello This is one.java page!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello This is one.java page!" );
+    }
+}
